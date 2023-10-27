dc44eaa7a5ed7d03787b6ca7536518cc5fc0baa2
mjaylaani <65979673+mjaylaani@users.noreply.github.com>
2023-10-27 09:26:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let burger =document.querySelector(".burger");
+let navigationBar =document.querySelector(".navigation-bar");
+
+burger.addEventListener("click",()=> {
+    navigationBar.classList.toggle('active');
+
+});
